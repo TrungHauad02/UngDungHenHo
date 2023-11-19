65f48834d5c12bf9521f0e1efbb8ce8afbaf1abc
Bách Trần <maitranbach2812@gmail.com>
2023-11-20 04:23:44 +0700

    bao cao

---
+++ UngDungHenHo/Forms/FormReport.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace UngDungHenHo.Forms
+{
+    partial class FormReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv_Report = new System.Windows.Forms.DataGridView();
+            this.btnXem = new System.Windows.Forms.Button();
+            this.btnDSChan = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCapNhat = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtIDQuantri = new System.Windows.Forms.TextBox();
+            this.txtNoiDung = new System.Windows.Forms.TextBox();
+            this.datePhanHoi = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtIDBaoCao = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Report)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv_Report
+            // 
+            this.dgv_Report.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv_Report.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv_Report.Location = new System.Drawing.Point(29, 123);
+            this.dgv_Report.Name = "dgv_Report";
+            this.dgv_Report.RowHeadersWidth = 51;
+            this.dgv_Report.RowTemplate.Height = 24;
+            this.dgv_Report.Size = new System.Drawing.Size(718, 247);
+            this.dgv_Report.TabIndex = 0;
+            this.dgv_Report.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_Report_CellClick);
+            // 
+            // btnXem
+            // 
+            this.btnXem.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXem.Location = new System.Drawing.Point(96, 394);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(87, 39);
+            this.btnXem.TabIndex = 1;
+            this.btnXem.Text = "Xem";
+            this.btnXem.UseVisualStyleBackColor = true;
+            this.btnXem.Click += new System.EventHandler(this.btnXem_Click);
+            // 
+            // btnDSChan
+            // 
+            this.btnDSChan.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDSChan.Location = new System.Drawing.Point(608, 394);
+            this.btnDSChan.Name = "btnDSChan";
+            this.btnDSChan.Size = new System.Drawing.Size(87, 39);
+            this.btnDSChan.TabIndex = 2;
+            this.btnDSChan.Text = "DS chặn";
+            this.btnDSChan.UseVisualStyleBackColor = true;
+            this.btnDSChan.Click += new System.EventHandler(this.btnDSChan_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(438, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 32);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Quản lý Báo cáo";
+            // 
+            // btnCapNhat
+            // 
+            this.btnCapNhat.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCapNhat.Location = new System.Drawing.Point(216, 394);
+            this.btnCapNhat.Name = "btnCapNhat";
+            this.btnCapNhat.Size = new System.Drawing.Size(87, 39);
+            this.btnCapNhat.TabIndex = 4;
+            this.btnCapNhat.Text = "Cập nhật";
+            this.btnCapNhat.UseVisualStyleBackColor = true;
+            this.btnCapNhat.Click += new System.EventHandler(this.btnCapNhat_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(781, 193);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "ID Quản trị";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(781, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ngày phản hồi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(781, 324);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(143, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Nội dung phản hồi";
+            // 
+            // txtIDQuantri
+            // 
+            this.txtIDQuantri.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtIDQuantri.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIDQuantri.Location = new System.Drawing.Point(984, 191);
+            this.txtIDQuantri.Multiline = true;
+            this.txtIDQuantri.Name = "txtIDQuantri";
+            this.txtIDQuantri.Size = new System.Drawing.Size(100, 29);
+            this.txtIDQuantri.TabIndex = 9;
+            // 
+            // txtNoiDung
+            // 
+            this.txtNoiDung.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNoiDung.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNoiDung.Location = new System.Drawing.Point(984, 316);
+            this.txtNoiDung.Multiline = true;
+            this.txtNoiDung.Name = "txtNoiDung";
+            this.txtNoiDung.Size = new System.Drawing.Size(200, 29);
+            this.txtNoiDung.TabIndex = 10;
+            // 
+            // datePhanHoi
+            // 
+            this.datePhanHoi.Location = new System.Drawing.Point(984, 258);
+            this.datePhanHoi.Name = "datePhanHoi";
+            this.datePhanHoi.Size = new System.Drawing.Size(200, 22);
+            this.datePhanHoi.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(781, 139);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 20);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "ID Báo cáo";
+            // 
+            // txtIDBaoCao
+            // 
+            this.txtIDBaoCao.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtIDBaoCao.Enabled = false;
+            this.txtIDBaoCao.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIDBaoCao.Location = new System.Drawing.Point(984, 131);
+            this.txtIDBaoCao.Multiline = true;
+            this.txtIDBaoCao.Name = "txtIDBaoCao";
+            this.txtIDBaoCao.Size = new System.Drawing.Size(100, 29);
+            this.txtIDBaoCao.TabIndex = 12;
+            // 
+            // FormReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1196, 462);
+            this.Controls.Add(this.txtIDBaoCao);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtNoiDung);
+            this.Controls.Add(this.txtIDQuantri);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.datePhanHoi);
+            this.Controls.Add(this.btnCapNhat);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDSChan);
+            this.Controls.Add(this.btnXem);
+            this.Controls.Add(this.dgv_Report);
+            this.Name = "FormReport";
+            this.Text = "FormReport";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv_Report)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv_Report;
+        private System.Windows.Forms.Button btnXem;
+        private System.Windows.Forms.Button btnDSChan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCapNhat;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtIDQuantri;
+        private System.Windows.Forms.TextBox txtNoiDung;
+        private System.Windows.Forms.DateTimePicker datePhanHoi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtIDBaoCao;
+    }
+}
--- UngDungHenHo/Program.cs
+++ UngDungHenHo/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UngDungHenHo.Forms;
 
 namespace UngDungHenHo
 {
@@ -16,7 +17,7 @@ namespace UngDungHenHo
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain());
+            Application.Run(new FormReport());
         }
     }
 }
