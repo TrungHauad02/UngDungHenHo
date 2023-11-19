0e372074a6e7f07653b559bf9c5fb47e0ea6b745
CapTan2003 <113422566+CapTan2003@users.noreply.github.com>
2023-11-20 01:25:10 +0700

    capnhatform

---
+++ UngDungHenHo/Forms/FormBaoCaoNguoiDung.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UngDungHenHo.Forms
+{
+    public partial class FormBaoCaoNguoiDung : Form
+    {
+        public FormBaoCaoNguoiDung()
+        {
+            InitializeComponent();
+        }
+
+        private void FormBaoCaoNguoiDung_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
